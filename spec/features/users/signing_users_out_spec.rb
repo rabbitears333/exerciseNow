6e1ba9131e6f11148a2593f08ea2f94ec066bc27
Andy Wang <rabbitears333@gmail.com>
2016-12-30 16:53:12 +0000

    implement sign out

---
+++ spec/features/users/signing_users_out_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Users sign out" do
+    before do
+        @john = User.create!(email: "john@example.com", password:"password")
+         visit "/"
+        click_link "Sign in"
+        fill_in "Email", with: @john.email
+        fill_in "Password", with: @john.password
+        click_button "Log In"
+    end
+    scenario do
+        visit "/"
+        click_link "Sign out"
+        expect(page).to have_content("You have signed out for account")
+    end
+end
