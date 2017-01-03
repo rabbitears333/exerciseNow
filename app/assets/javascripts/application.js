0008762332cba303814537b0404f0cc0bb1d2fd3
Andy Wang <rabbitears333@gmail.com>
2017-01-03 19:58:05 +0000

    Add graph

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery-ui/datepicker
 //= require jquery_ujs
 //= require bootstrap
+//= require d3
 // require turbolinks
 //= require_tree .
--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('#exercise_workout_date').datepicker({dateFormat: 'yy-mm-dd'});
+    
+    var regex = /\/users\/\d+\/exercises$/i;
+    
+    if($(location).attr('pathname').match(regex)){
+        drawchart();
+    }
 });
