57f7f8b00b045b3ad5519164041ee08e65a261f4
Andy Wang <rabbitears333@gmail.com>
2017-01-03 19:14:06 +0000

    display users workout

---
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "Listing exercises" do
+    before do
+        @john = User.create!(email: "john@example.com", password: "password")
+        login_as(@john)
+        @e1 = @john.exercises.create(duration_in_min: 20, 
+                                    workout: "My activity"
+                                    workout_date: Date.today)
+        @e2 = @john.exercises.create(duration_in_min: 50, 
+                                    workout: "My lifting"
+                                    workout_date: 2.days.ago)
+        
+    end
+    scenario "with invalid credentials" do
+        visit "/"
+        click_link "My lounge"
+        
+        expect(page).to have_content(@e1.duration_in_min)
+        expect(page).to have_content(@e1.workout)
+        expect(page).to have_content(@e1.workout_date)
+        
+        expect(page).to have_content(@e2.duration_in_min)
+        expect(page).to have_content(@e2.workout)
+        expect(page).to have_content(@e2.workout_date)
+        
+        # expect(page).to have_content(@e3.duration_in_min)
+        # expect(page).to have_content(@e3.workout)
+        # expect(page).to have_content(@e3.workout_date)
+    end
+end
