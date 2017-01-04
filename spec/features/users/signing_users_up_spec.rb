ef21c627196c53b4b8e8a4c1e5055c0e543386e6
Andy Wang <rabbitears333@gmail.com>
2017-01-04 22:24:33 +0000

    valid

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -15,4 +15,18 @@ RSpec.feature "Users sign up" do
         visit "/"
         expect(page).to have_content("John Doe")
     end
+    
+    scenario "with invalid user credentials" do
+        visit "/"
+        click_link "Sign up"
+        fill_in "First name", with: ""
+        fill_in "Last name", with: ""
+        fill_in "Email", "john@example.com"
+        fill_in "Password", with: "password"
+        fill_in "Password Confirmation", with: "password"
+        click_button "Sign up"
+        expect(page).to have_content("first name cannot be blank")
+        
+        expect(page).to have_content("last name cannot be blank")
+    end
 end
